598b422c239cb27bab028c2460bd43a1292d17a4
Kolchaks <ondatr23@gmail.com>
2023-05-08 21:13:44 +0300

    try_8

--- MainApp/views.py
+++ MainApp/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, HttpResponse
 from django.http import HttpResponseNotFound
 import json
 
-
 # Create your views here.
 
 author = {
@@ -10,18 +9,19 @@ author = {
     "surname": "Kolchak",
 }
 
+with open('countries.json', 'r') as read_file:
+    countries = json.load(read_file)
+
+
 def home(request):
-    context = {
-        "name": author['name'],
-        "surname": author['surname']
-    }
+    context = {"name": 'Хватит лениться!'}
     return render(request, 'index.html', context)
 
+
 def about(request):
-    context = {
-        'author': author
-    }
+    context = {'author': author}
     return render(request, 'about.html', context)
+
 
 def countries_list(request):
     with open("countries.json", "r") as read_file:
@@ -29,14 +29,14 @@ def countries_list(request):
 
     for country in countries_json:
         if countries_json['country'] == country:
-            context = {
-                'country': country
-            }
+            context = {'country': country}
+
             return render(request, 'countries_list.html', context)
 
 
 def countries_page(request):
     pass
+
 
 def languages(request):
     pass
